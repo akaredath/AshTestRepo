a9b803e866dd288ef9222694473509fab002d3c6
akaredath <aishahkaredath7@gmail.com>
2016-08-24 19:56:11 -0500

    ashwin added hello world project

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)HelloWorld.java
+ *
+ * HelloWorld application
+ *
+ * @author 
+ * @version 1.00 2016/8/24
+ */
+ 
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
